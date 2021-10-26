a96fe3e9c925218d77449268ec456cb06f2debd7
roman <charitonov@ya.ru>
2021-10-26 21:00:59 +0300

    SaSSddssss

--- PDF_downL.py
+++ PDF_downL.py
@@ -1,13 +1,13 @@
 import wget
 import requests
-url = 'https://stepik.org/certificate/cdb9b191e6ac2fd45966961498a21064070ef4b7.pdf'
+url = 'https://stepik.org/certificate/f976f873d5ce3843ec92872700d17d226c0f320d.pdf'
 resp = requests.get(url, stream=True)
 
-with open('/home/roman/testDir/myfile.pdf', 'wb') as f:
+with open('/home/roman/testDir/Stepik_Inf_sec_Diploma.pdf', 'wb') as f:
     f.write(resp.content)
 
 # link = 'https://stepik.org/certificate/2b03ea3b8e235d0ad83432f9aceddb45915006da.pdf'
 # obj = wget.download(link)
-# with open('/home/roman/testDir/myfile1.pdf', 'wb') as f:
-#     f.write(obj)
-
+# with open('/home/roman/testDir/myfile1.pdf', 'wb') as file:
+#    file .write(obj.content)
+#
